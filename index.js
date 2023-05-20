56ff30776833d75957f8972a5e8d9aeedd91ea1e
epsitarif <ariful11233@gmail.com>
2023-05-21 01:40:57 +0600

    add query to get data with email

--- index.js
+++ index.js
@@ -36,6 +36,16 @@ async function run() {
       res.send(result);
     })
 
+    app.get('/toys', async(req, res) =>{
+      console.log(req.query.email);
+      let query = {};
+      if (req.query?.email) {
+        query = { email: req.query.email }
+      }
+      const result = await toyCollection.find(query).toArray();
+      res.send(result);
+    })
+
     app.get('/toys/:id', async(req, res) =>{
       const id = req.params.id 
       const filter = {_id:new ObjectId(id)}
